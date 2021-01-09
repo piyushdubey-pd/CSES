b68fc58cd5e17853d67dd9466b51264d05d3e739
piyushdubey-pd <piyushdubey72090@gmail.com>
2021-01-09 12:55:34 +0530

    Weird Algo Successful

--- Introductory/WeirdAlgo.cpp
+++ Introductory/WeirdAlgo.cpp
@@ -1,1 +1,9 @@
-"DS"
+#include <bits/stdc++.h>
+using ll=long long;
+using namespace std;
+int main(){
+    ll n;cin>>n;
+    while(n!=1)
+    {cout<<n<<" ";n=(n&1)?(n*3+1):(n/2);}
+    cout<<1<<endl;
+}
