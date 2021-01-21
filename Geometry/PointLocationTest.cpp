4406c82ffc2721c8bd0ea1aedc6d1d7f107aa190
piyushdubey-pd <piyushdubey72090@gmail.com>
2021-01-21 22:12:45 +0530

    Re- commit Point location

--- Geometry/PointLocationTest.cpp
+++ Geometry/PointLocationTest.cpp
@@ -1,10 +1,11 @@
 #include<bits/stdc++.h>
 using namespace std;
+using ll=long long;
 int main(){
-    int t;
+    ll t;
     cin>>t;
     while(t--){
-    int x1,x2,x3,y1,y2,y3;
+    ll x1,x2,x3,y1,y2,y3;
     cin>>x1>>y1>>x2>>y2>>x3>>y3;
     x2-=x1;x3-=x1;
     y2-=y1;y3-=y1;
